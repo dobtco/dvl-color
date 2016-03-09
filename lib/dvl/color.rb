33fb8bdb21c634f83e0486b133df59c892c6fbcc
Adam Becker <ad@mbecker.cc>
2016-03-09 15:21:46 -0500

    i am such an asshole

--- lib/dvl/color.rb
+++ lib/dvl/color.rb
@@ -1,10 +1,10 @@
 require 'chroma'
 require 'chroma_wcag_contrast'
 
-require 'dvl/color/fake_rgba'
-require 'dvl/color/generator'
-
 module Dvl
   module Color
   end
 end
+
+require 'dvl/color/fake_rgba'
+require 'dvl/color/generator'
